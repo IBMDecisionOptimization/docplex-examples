5a335863c6140a2f3428bc1be6081c2dbbf93828
viulong-kong <viulong.kong@fr.ibm.com>
2016-02-09 10:36:59 +0100

    Removing Byte Order Marks

--- examples/cp/basic/n_queen.py
+++ examples/cp/basic/n_queen.py
@@ -1,4 +1,4 @@
-﻿# --------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 # Source file provided under Apache License, Version 2.0, January 2004,
 # http://www.apache.org/licenses/
 # (c) Copyright IBM Corp. 2015, 2016
